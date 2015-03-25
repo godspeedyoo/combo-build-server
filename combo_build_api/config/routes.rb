a93b68565e5fb784220416370f1b768a9ab79a9d
Joseph Won <godspeedyoo@gmail.com>
2015-03-24 17:57:26 -0700

    Add rspec test for accept headers matching version

--- combo_build_api/config/routes.rb
+++ combo_build_api/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # API definition
-  namespace :api, defaults: { format: :json } do
+  namespace :api, defaults: { format: :json },
+                            constraints: { subdomain: 'api' },
+                            path: '/' do
     # set scope for api versioning
     scope module: :v1 do
 
